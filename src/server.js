f7be1d10258d289ff0ba98841b4b3f534c7e8668
uravilla <ushmitharavilla998@gmail.com>
2022-10-10 07:31:05 -0400

    updates

--- src/server.js
+++ src/server.js
@@ -12,9 +12,63 @@ const port = process.env.PORT || 3000;
 
 
 // The main page of our website
+//app.get('/', (req, res) => {
+//  res.send('Hello World!')
+//});
+app.use(express.static('public'));
+const cookieParser = require('cookie-parser');
+app.set('view engine', 'ejs');
+
+/*const {encode} = require('html-entities');
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+const name = req.query.name || "World";
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <title>An Example Title</title>
+        <link rel="stylesheet" href="app.css">
+      </head>
+      <body>
+         <h1>Hello, ${encode(name)}!</h1>
+        <p>HTML is so much better than a plain string!</p>
+      </body>
+    </html>
+  `);
 });
+*/
+app.use(cookieParser());
+let nextVisitorId = 1;
+let visitormsg = ""
+app.get('/', (req, res) => {
+  let lasttimevisit = req.cookies.visited;
+  let visitorId = req.cookies.visitorId;
+
+  if (lasttimevisit == undefined) {
+     visitormsg = "You have never visited before."
+  }
+  else{
+    lasttimevisit = Math.floor((Date.now() - req.cookies.visited) / 1000);
+      visitormsg = `It has been ${lasttimevisit} seconds since your last visit`;
+  }
+
+  if (req.cookies['visitorId']) {
+      res.cookie('visitorId', nextVisitorId);
+  } else {
+      res.cookie('visitorId',nextVisitorId++);
+  }
+
+  res.cookie('visitorId', visitorId);
+  res.cookie('visited', Date.now());
+  res.render('welcome', {
+      name: req.query.name || "World",
+      lasttimevisit: lasttimevisit,
+      visitorId: nextVisitorId,
+      visitormsg: visitormsg
+    });
+    console.log(req.cookies);
+  });
 
 // Start listening for network connections
 app.listen(port);
